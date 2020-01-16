24de44475fe0a3fa2d1ee4f74d26c8a8946ca9d8
Tom Poulter <tom.poulter.1@outlook.com>
2020-01-16 20:55:48 +0000

    rails first example

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  get 'restaurants', to: 'restaurants#index'
+
+  post 'restaurants', to: 'restaurants#create'
+
+  root to: 'pages#home'
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
